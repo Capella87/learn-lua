d2d9ba2c11b10db096615de559fc28e53127b24a
Capella87 <contact@capelladev.com>
2023-08-13 00:01:18 +0900

    Rename the extension lua to luau

--- hello.lua
+++ hello.lua
@@ -1,1 +1,14 @@
+-- Learning Lua Language (Precisely Luau based on Lua 5.1 by Roblox)
+
+-- Print a message
 print("Hello World!")
+
+-- Comment can be written like this.
+
+-- We don't have to write semicolons every end of line like Python.
+a = 1
+d = 1;
+
+a = a + d
+
+print(a)
